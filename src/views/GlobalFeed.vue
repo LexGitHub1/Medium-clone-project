9ee54dc9769ca55ef2a11d800d1b8249197de657
Alexander Pankov <alexander.pankov88@mail.ru>
2021-01-16 02:03:46 +0700

    Начало работы с Feed

---
+++ src/views/GlobalFeed.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home-page">
+    BANNER
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <mcp-feed :api-url="apiUrl" />
+        </div>
+        <div class="col-md-3">
+          POPULAR TAGS
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import McpFeed from '@/components/Feed'
+export default {
+  name: 'McpGlobalFeed',
+  components: {
+    McpFeed
+  },
+  data() {
+    return {
+      apiUrl: '/articles'
+    }
+  }
+}
+</script>
