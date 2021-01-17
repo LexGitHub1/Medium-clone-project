e00eda3136544c7a7585380219e54a6629c2dbb5
Alexander Pankov <alexander.pankov88@mail.ru>
2021-01-17 14:32:05 +0700

    Namespacing

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,8 @@ import article from '@/store/modules/article'
 import createArticle from '@/store/modules/createArticle'
 import editArticle from '@/store/modules/editArticle'
 import settings from '@/store/modules/settings'
+import foo from '@/store/modules/foo'
+import bar from '@/store/modules/bar'
 
 Vue.use(Vuex)
 
@@ -22,6 +24,8 @@ export default new Vuex.Store({
     article,
     createArticle,
     editArticle,
-    settings
+    settings,
+    foo,
+    bar
   }
 })
